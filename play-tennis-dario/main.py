7ab88deecac1a752b726c014cb4acdee8b463c63
dario2434 <113589458+dario2434@users.noreply.github.com>
2024-11-12 14:08:38 +0000

    Update main.py

--- play-tennis-dario/main.py
+++ play-tennis-dario/main.py
@@ -1,67 +1,68 @@
 def run(points: str) -> str:
     a_points = 0
     b_points = 0
-    a_games_win = 0
-    b_games_win = 0
+    a_games = 0
+    b_games = 0
     sets_a = 0
     sets_b = 0
     result = ''
-    tiebreak = False  # Variable para gestionar el tiebreak
+    tiebreak = False 
+    tiebreak_a_points = 0  
+    tiebreak_b_points = 0  
 
-    # Recorriendo todos los puntos
-    for punto in points:
-        if punto == 'A':
-            a_points += 1
-        elif punto == 'B':
-            b_points += 1
-
-        # El juego se gana con 4 puntos o diferencia de dos puntos
-        if a_points >= 4 and a_points - b_points >= 2:
-            a_games_win += 1
-            a_points = 0
-            b_points = 0
-        elif b_points >= 4 and b_points - a_points >= 2:
-            b_games_win += 1
-            a_points = 0
-            b_points = 0
-
-        #se activa el tiebreak cuando el juego va 6-6
-        if a_games_win == 6 and b_games_win == 6:
-            tiebreak = True
-
-        # Si estamos en un tiebreak
+    for point in points:
         if tiebreak:
-            if a_points >= 7 and a_points - b_points >= 2:
+            if point == 'A':
+                tiebreak_a_points += 1
+            elif point == 'B':
+                tiebreak_b_points += 1
+
+            if tiebreak_a_points >= 7 and tiebreak_a_points - tiebreak_b_points >= 2:
                 sets_a += 1
-                result += f'7-6 '
-                a_games_win = 0
-                b_games_win = 0
-                a_points = 0
-                b_points = 0
+                result += '7-6 '
                 tiebreak = False
-            elif b_points >= 7 and b_points - a_points >= 2:
+                a_games = b_games = 0
+                tiebreak_a_points = 0
+                tiebreak_b_points = 0
+            elif tiebreak_b_points >= 7 and tiebreak_b_points - tiebreak_a_points >= 2:
                 sets_b += 1
-                result += f'6-7 '
-                a_games_win = 0
-                b_games_win = 0
+                result += '6-7 '
+                tiebreak = False
+                a_games = b_games = 0
+                tiebreak_a_points = 0
+                tiebreak_b_points = 0
+        else:
+            if point == 'A':
+                a_points += 1
+            elif point == 'B':
+                b_points += 1
+
+            if a_points >= 4 and a_points - b_points >= 2:
+                a_games += 1
                 a_points = 0
                 b_points = 0
-                tiebreak = False  
-        else:
-            if a_games_win >= 6 and a_games_win - b_games_win >= 2:
+            elif b_points >= 4 and b_points - a_points >= 2:
+                b_games += 1
+                a_points = 0
+                b_points = 0
+
+            if a_games >= 6 and a_games - b_games >= 2:
                 sets_a += 1
-                result += f'{a_games_win}-{b_games_win} '
-                a_games_win = 0
-                b_games_win = 0
-            elif b_games_win >= 6 and b_games_win - a_games_win >= 2:
+                result += f'{a_games}-{b_games} '
+                a_games = b_games = 0
+            elif b_games >= 6 and b_games - a_games >= 2:
                 sets_b += 1
-                result += f'{a_games_win}-{b_games_win} '
-                a_games_win = 0
-                b_games_win = 0
+                result += f'{a_games}-{b_games} '
+                a_games = b_games = 0
 
-        if sets_a == 2 or sets_b == 2:
-            break
-
+            if a_games == 6 and b_games == 6:
+                tiebreak = True
+                tiebreak_a_points = 0
+                tiebreak_b_points = 0
+                continue
+            
+            if sets_a == 2 or sets_b == 2:
+                break
     return result
 
 # DO NOT TOUCH THE CODE BELOW
